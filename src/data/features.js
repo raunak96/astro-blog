618b1c1ccce79237a0841536651f9e7468a7b429
Raunak Kedia <raunakkedia1@gmail.com>
2022-10-15 21:10:10 +0530

    made Showcase, Features, Card and Tabs Component UI

---
+++ src/data/features.js
@@ -0,0 +1,16 @@
+const featuresData = [
+	{
+		title: "Zero JavaScript Runtime",
+		body: " Astro renders HTML on the server and strips away any remaining, unused JavaScript.",
+	},
+	{
+		title: "The Power of Islands",
+		body: " Need interactive UI? Load individual, non-blocking component islands in parallel.",
+	},
+	{
+		title: "Lazy-Loading Islands",
+		body: "Components only hydrate when they scroll into view. If you don't see it, Astro won't load it.",
+	},
+];
+
+export default featuresData;
